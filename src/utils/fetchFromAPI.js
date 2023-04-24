b565b99fbf6a4ca05397f33da17a37278dbe79cf
abdo887 <abdoraga52@gmail.com>
2023-04-24 19:28:20 +0200

    adding final stages

---
+++ src/utils/fetchFromAPI.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+export const BASE_URL = 'https://youtube-v31.p.rapidapi.com';
+
+const options = {
+  params: {
+    maxResults: 50,
+  },
+  headers: {
+    'X-RapidAPI-Key': '59ebfd8f34mshb89ed0b8cfe49edp147109jsnb00172a30964',
+    'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com',
+  },
+};
+
+export const fetchFromAPI = async (url) => {
+  const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+
+  return data;
+};
